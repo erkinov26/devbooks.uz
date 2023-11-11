2bfaaa2507eeacc06feed7baeaf966d0503df781
erkinov26 <erkinov0247@gmail.com>
2023-11-11 18:16:38 +0500

    awd

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import authors from "./Data/data";
 import { useState } from "react";
 import SignUp from "./Pages/SignUp";
 // import LogIn from "./Pages/LogIn";
+import LogIn from "./Pages/LogIn";
 import MyProfile from "./Pages/MyProfile";
 function App() {
   const [data, setData] = useState(authors);
@@ -46,6 +47,7 @@ function App() {
             element={<Forum open={open} setOpen={setOpen} />}
           />
           {/* <Route path="/" element={<LogIn />} /> */}
+          <Route path="/" element={<LogIn />} />
           <Route path="/SignUp" element={<SignUp />} />
 
           <Route path="/myProfile" element={<MyProfile />} />
--- src/Pages/login.js
+++ src/Pages/login.js
@@ -1,60 +1,60 @@
-// import React, { useRef } from "react";
-// import { useNavigate } from "react-router-dom";
-// import img from "../images/logInLeft.svg";
-// import userData from "../Data/UserData";
+import React, { useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import img from "../images/logInLeft.svg";
+import userData from "../Data/UserData";
 
-// const LogIn = () => {
-//   const navigate = useNavigate();
-//   const emailRef = useRef();
-//   const passwordRef = useRef();
-//   const onsubmit = (e) => {
-//     e.preventDefault();
+function LogIn () {
+  const navigate = useNavigate();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const onsubmit = (e) => {
+    e.preventDefault();
 
-//     // eslint-disable-next-line array-callback-return
-//     userData.filter((user) => {
-//       if (
-//         user.email === emailRef.current.value.trim() &&
-//         user.password === passwordRef.current.value.trim()
-//       ) {
-//         return navigate("/bosh_sahifa");
-//       }
-//     });
-//   };
-//   return (
-//     <div className="logInSection">
-//       <div className="logInLeft">
-//         <img src={img} alt={img} className="logInLeftImg" />
-//       </div>
-//       <div className="logInRight">
-//         <form className="logInForm" onSubmit={onsubmit}>
-//           <h1 className="logInTitle">Sign in</h1>
-//           <p className="questionText">
-//             Do not you have an account ?{" "}
-//             <span
-//               className="innerQuestionText"
-//               onClick={() => navigate("/SignUp")}
-//             >
-//               Sign up
-//             </span>
-//           </p>
-//           <input
-//             ref={emailRef}
-//             className="FormInput"
-//             placeholder="Email"
-//             type="email"
-//           />
-//           <input
-//             ref={passwordRef}
-//             className="FormInput"
-//             placeholder="Password"
-//             type="text"
-//           />
-//           <button className="logBtn" type="submit">
-//             Next step
-//           </button>
-//         </form>
-//       </div>
-//     </div>
-//   );
-// };
-// export default LogIn;
+    // eslint-disable-next-line array-callback-return
+    userData.filter((user) => {
+      if (
+        user.email === emailRef.current.value.trim() &&
+        user.password === passwordRef.current.value.trim()
+      ) {
+        return navigate("/bosh_sahifa");
+      }
+    });
+  };
+  return (
+    <div className="logInSection">
+      <div className="logInLeft">
+        <img src={img} alt={img} className="logInLeftImg" />
+      </div>
+      <div className="logInRight">
+        <form className="logInForm" onSubmit={onsubmit}>
+          <h1 className="logInTitle">Sign in</h1>
+          <p className="questionText">
+            Do not you have an account ?{" "}
+            <span
+              className="innerQuestionText"
+              onClick={() => navigate("/SignUp")}
+            >
+              Sign up
+            </span>
+          </p>
+          <input
+            ref={emailRef}
+            className="FormInput"
+            placeholder="Email"
+            type="email"
+          />
+          <input
+            ref={passwordRef}
+            className="FormInput"
+            placeholder="Password"
+            type="text"
+          />
+          <button className="logBtn" type="submit">
+            Next step
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+export default LogIn;
